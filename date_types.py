a3e80076f149994bc9963eb985f91cd5230d2099
Alexander Minaku <al.motologa@gmail.com>
2023-01-22 21:25:45 +0200

    HW

---
+++ date_types.py
@@ -0,0 +1,29 @@
+i = -10
+f = 0.001
+s = 'abc'
+l = [1,2,3]
+d = { 'a': 1, 'b': 2, 'c': 3 }
+b = False
+
+x = f > i
+
+print( type( i ) )
+print( "Variabila i este de tip 'int'\n")
+
+print( type( f ) )
+print( "Variabila f este de tip 'float'\n")
+
+print( type( s ) )
+print( "Variabila s este de tip 'str'\n")
+
+print( type( l ) )
+print( "Variabila l este de tip 'list'\n")
+
+print( type( d ) )
+print( "Variabila d este de tip 'dict'\n")
+
+print( type( b ) )
+print( "Variabila b este de tip 'bool'\n")
+
+print( type( x ) )
+print( "Variabila x este de tip 'bool'\n")
